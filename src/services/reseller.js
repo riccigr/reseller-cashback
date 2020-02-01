0366447052006509d766144685b888b83a63e435
riccigr <ricci.gr@gmail.com>
2020-02-01 18:18:12 -0300

    start validanting reseller

--- src/services/reseller.js
+++ src/services/reseller.js
@@ -1,9 +1,22 @@
-module.exports = (app) =>  {
-    const service = {};
 
-    service.create = (request, response) => {
-        response.status(201).json('OK');
+const service = {};
+
+service.create = (request, response) => {
+
+    request.assert('reseller.full_name', 'Full name is mandatory').notEmpty();
+    request.assert('reseller.cpf', 'CPF is mandatory').notEmpty();
+    request.assert('reseller.email', 'E-mail is mandatory').notEmpty()
+    request.assert('reseller.password', 'Password is mandatory').notEmpty()
+
+    const invalid = request.validationErrors();
+    if(invalid){
+        response.status(400).send(invalid);
+        return;
     }
 
+    response.status(201).json('OK');
+}
+
+module.exports = (app) =>  {
     return service;
 }
