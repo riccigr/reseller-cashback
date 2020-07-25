f9e449f7226867269de0bdfb10f1022ebc84dcf9
Ricci <ricci.gr@gmail.com>
2020-07-25 20:48:43 -0300

    fixing lint

--- src/dao/purchaseDAO.js
+++ src/dao/purchaseDAO.js
@@ -1,26 +1,28 @@
+const logger = require('../helper/logger');
+
 function purchaseDAO(connection) {
-  this._connection = connection;
+  this.dbConnection = connection;
 }
 
-purchaseDAO.prototype.save = function(purchase, callback) {
-  console.log(purchase);
-  this._connection.query('INSERT INTO compra SET ?', purchase, callback);
+purchaseDAO.prototype.save = function save(purchase, callback) {
+  logger.info(purchase);
+  this.dbConnection.query('INSERT INTO compra SET ?', purchase, callback);
 };
 
-purchaseDAO.prototype.update = function(purchase, id, callback) {
-  this._connection.query('UPDATE compra SET ? where codigo = ? ', [purchase, id], callback);
+purchaseDAO.prototype.update = function update(purchase, id, callback) {
+  this.dbConnection.query('UPDATE compra SET ? where codigo = ? ', [purchase, id], callback);
 };
 
-purchaseDAO.prototype.remove = function(id, callback) {
-  this._connection.query('DELETE FROM compra where codigo = ? ', [id], callback);
+purchaseDAO.prototype.remove = function remove(id, callback) {
+  this.dbConnection.query('DELETE FROM compra where codigo = ? ', [id], callback);
 };
 
-purchaseDAO.prototype.getById = function(id, callback) {
-  this._connection.query('SELECT * FROM compra WHERE codigo = ? LIMIT 1', [id], callback);
+purchaseDAO.prototype.getById = function getById(id, callback) {
+  this.dbConnection.query('SELECT * FROM compra WHERE codigo = ? LIMIT 1', [id], callback);
 };
 
-purchaseDAO.prototype.getByCpf = function(id, callback) {
-  this._connection.query(
+purchaseDAO.prototype.getByCpf = function getByCpf(id, callback) {
+  this.dbConnection.query(
     `SELECT c.codigo, c.valor, c.data, c.cpf, c.valor_cashback, c.porcentagem_cashback, sc.descricao 
                             FROM compra c LEFT JOIN status_compra sc on c.status = sc.status WHERE cpf = ?`,
     [id],
--- src/dao/resellerDAO.js
+++ src/dao/resellerDAO.js
@@ -1,13 +1,13 @@
 function resellerDAO(connection) {
-  this._connection = connection;
+  this.dbConnection = connection;
 }
 
-resellerDAO.prototype.save = function(reseller, callback) {
-  this._connection.query('INSERT INTO revendedor SET ?', reseller, callback);
+resellerDAO.prototype.save = function save(reseller, callback) {
+  this.dbConnection.query('INSERT INTO revendedor SET ?', reseller, callback);
 };
 
-resellerDAO.prototype.getById = function(id, callback) {
-  this._connection.query('SELECT * FROM revendedor WHERE cpf = ? LIMIT 1', [id], callback);
+resellerDAO.prototype.getById = function getById(id, callback) {
+  this.dbConnection.query('SELECT * FROM revendedor WHERE cpf = ? LIMIT 1', [id], callback);
 };
 
 module.exports = () => {
